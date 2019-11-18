c19c6e09db665923aad6306bb76e39147262cc59
RylanFerrer <rylanferrer@gmail.com>
2019-11-17 22:12:59 -0800

    Updates

---
+++ precache-manifest.bede7b4a47dcf74e650dabe0c04dcbd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf0a9d6ca7e583e94f7d384c636b27e",
+    "url": "/PortfolioNew/index.html"
+  },
+  {
+    "revision": "f86ed2bec055fafba4e3",
+    "url": "/PortfolioNew/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2b81a8a861384393ae16",
+    "url": "/PortfolioNew/static/js/2.e50112b1.chunk.js"
+  },
+  {
+    "revision": "f86ed2bec055fafba4e3",
+    "url": "/PortfolioNew/static/js/main.1de8fe8e.chunk.js"
+  },
+  {
+    "revision": "bd91bea465fe524ae42b",
+    "url": "/PortfolioNew/static/js/runtime-main.877cd599.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/PortfolioNew/static/media/logo.25bf045c.svg"
+  }
+]);
